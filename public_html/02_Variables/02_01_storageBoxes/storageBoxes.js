ea75011e6476624afb0a09c68d320eed2aa8873c
LiamOSullivan <lmosulli@tcd.ie>
2016-12-07 16:48:02 +0000

    More fix for print()

--- public_html/02_Variables/02_01_storageBoxes/storageBoxes.js
+++ public_html/02_Variables/02_01_storageBoxes/storageBoxes.js
@@ -10,18 +10,18 @@ function setup() {
     println(boxA); //read the value from box a and println it out - NOTICE NO DOUBLE QUOTES
     println(boxB);
 
-    println("The value in boxA is: " + boxA); //read the value from box a and println it out with a message as preamble
-    println("The value in boxB is: " + boxB);
+    println("The value in boxA is: " +boxA); //read the value from box a and println it out with a message as preamble
+    println("The value in boxB is: " +boxB);
 
     boxA = -1234;
     boxB = 5678;
 
-    println("The value in Box A has changed to: " + boxA); //read the current value from box a and println it out with a message as preamble
-    println("The value in Box B has changed to: " + boxB);
+    println("The value in Box A has changed to: "+boxA); //read the current value from box a and println it out with a message as preamble
+    println("The value in Box B has changed to: "+boxB);
 
     boxA = boxB; //take the value from boxB and put it in BoxA
 
-    println("The value in Box A has changed again, to: " + boxA);
-    println("The value in Box B has now changed to: " + boxB);
+    println("The value in Box A has changed again, to: "+boxA);
+    println("The value in Box B has now changed to: "+boxB);
 
 }
