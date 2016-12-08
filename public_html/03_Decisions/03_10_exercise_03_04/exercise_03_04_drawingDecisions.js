9576fd9e55056788bcd764157f7ee41ca1257451
LiamOSullivan <lmosulli@tcd.ie>
2016-12-08 15:17:48 +0000

    More code content for Lesson 3 -Decisions. First run complete now.

---
+++ public_html/03_Decisions/03_10_exercise_03_04/exercise_03_04_drawingDecisions.js
@@ -0,0 +1,43 @@
+/*************************
+ * Foundations in Coding *       
+ *************************
+ *  IACT ** www.iact.ie  *
+ *************************/
+function setup() {
+    var colorShapesRed = false;
+    var addOutline = true;
+    createCanvas(400, 400);
+    background(0);
+    //drawing defaults
+    fill(255);
+    strokeWeight(5);
+    noStroke();
+    //if both true use a red fill and a white outline
+    if (colorShapesRed && addOutline) {
+        stroke(255);
+        fill(255, 0, 0);
+    }
+    //if a specifc one is true
+    else if (colorShapesRed || addOutline) {
+        if (!addOutline) {
+            noStroke();
+        }
+        else {
+            stroke(255);
+        }
+
+        if (colorShapesRed) {
+            fill(255, 0, 0);
+        }
+        else{
+            fill(0);
+        }
+
+    }
+    //if both false
+    else {
+        //do nothinhg
+    }
+    ellipse(width / 2, height / 2, 100, 100);
+}
+
